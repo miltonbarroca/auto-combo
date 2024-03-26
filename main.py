d85ddb8351780411f6a572d4d10dc48673622cca
Milton Barroca <miltonbarroca@gmail.com>
2024-03-26 11:28:02 -0300

    nova def mana

--- main.py
+++ main.py
@@ -4,58 +4,39 @@ import time
 from pynput import keyboard as kb
 import random
 
-def verifica_cor_na_area(x, y, largura, altura, cor):
-    """
-    Verifica se a cor está presente em uma área retangular de pixels.
-    
-    :param x: coordenada x do canto superior esquerdo da área
-    :param y: coordenada y do canto superior esquerdo da área
-    :param largura: largura da área
-    :param altura: altura da área
-    :param cor: tupla representando a cor (R, G, B)
-    :return: True se a cor for encontrada, False caso contrário
-    """
-    for i in range(x, x + largura):
-        for j in range(y, y + altura):
-            if pg.pixelMatchesColor(i, j, cor):
-                return True
-    return False
-
 def exura():
     print('Executando Exura')
     while programa.executando:
         if not programa.pausado:
             try:
-                if verifica_cor_na_area(759, 794, 5, 5, (73, 74, 74)):
+                if pg.pixelMatchesColor(759, 794, (73, 74, 74)):
                     pg.press('2') #hotkey exura
             except pg.FailSafeException:
                 pass
-        time.sleep(random.uniform(1.9, 2.8))  # Ajusta o delay conforme necessário
+        time.sleep(1.9)  # Ajusta o delay conforme necessário
 
 def mana():
     print('Verificando Mana')
     while programa.executando:
         if not programa.pausado:
             try:
-                image_location = pg.locateOnScreen('img/mana.jpg', confidence=0.3)
-                if image_location:
-                    pg.press('3') #hotkey pot mana
+                while not pg.locateOnScreen('img/mana_cheia.jpg'):
+                    pg.press('3')  # Pressiona a tecla "3"
+                    time.sleep(0.1)  # Espera um curto período antes de verificar novamente
             except pg.FailSafeException:  
                 pass
-        time.sleep(random.uniform(2.3, 2.8))  # Ajusta o delay conforme necessário
-
+        time.sleep(2.1)  # Ajusta o delay conforme necessário
 
 def life():
     print('Verificando Life')
     while programa.executando:
         if not programa.pausado:
             try:
-                if verifica_cor_na_area(605, 792, 5, 5, (69, 70, 70)):
+                if pg.pixelMatchesColor(605, 792, (69, 70, 70)):
                     pg.press('1') #hotkey pot vida
             except pg.FailSafeException:
                 pass
         time.sleep(1) # Ajusta o delay conforme necessário
-
 
 class MeuPrograma:
     def __init__(self):
@@ -82,9 +63,9 @@ class MeuPrograma:
                 print("Programa iniciado")
 
                 threading.Thread(target=self.loop_kill_box).start()
-#                threading.Thread(target=exura).start()  
-                threading.Thread(target=mana).start()   
-#                threading.Thread(target=life).start()   
+                threading.Thread(target=exura).start()  # Inicia a função exura em uma thread separada
+                threading.Thread(target=mana).start()   # Inicia a função mana em uma thread separada
+                threading.Thread(target=life).start()   # Inicia a função life em uma thread separada
             elif key.char == 'p':
                 self.pausado = not self.pausado
                 print("Programa pausado" if self.pausado else "Programa retomado")
