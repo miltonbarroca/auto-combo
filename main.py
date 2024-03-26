981d1ebd737a0e338a7f7cca9da14fc521086e70
Milton Barroca <miltonbarroca@gmail.com>
2024-03-26 10:02:06 -0300

    .

--- main.py
+++ main.py
@@ -4,41 +4,39 @@ import time
 from pynput import keyboard as kb
 import random
 
-# Definições das funções exura, mana e life
 def exura():
     print('Executando Exura')
-    while programa.executando:  # Continua executando enquanto o programa estiver em execução
+    while programa.executando:
         if not programa.pausado:
             try:
-                if pg.pixelMatchesColor(759, 794, (73, 74, 74)):  # Passa os valores diretamente
-                    pg.press('2')
-            except pg.FailSafeException:  # Trata exceção quando o pixel não é encontrado
+                if pg.pixelMatchesColor(759, 794, (73, 74, 74)):
+                    pg.press('2') #hotkey exura
+            except pg.FailSafeException:
                 pass
-        time.sleep(1.9)  # Ajusta o delay conforme necessário
+        time.sleep(random.uniform(1.9, 2.8))  # Ajusta o delay conforme necessário
 
 def mana():
     print('Verificando Mana')
-    while programa.executando:  # Continua executando enquanto o programa estiver em execução
+    while programa.executando:
         if not programa.pausado:
             try:
-                if pg.pixelMatchesColor(1073, 794, (121, 121, 121)):  # Passa os valores diretamente
-                    pg.press('3')
-            except pg.FailSafeException:  # Trata exceção quando o pixel não é encontrado
+                if pg.pixelMatchesColor(1073, 794, (121, 121, 121)):
+                    pg.press('3') #hotkey pot mana
+            except pg.FailSafeException:  
                 pass
-        time.sleep(2.1)  # Ajusta o delay conforme necessário
+        time.sleep(random.uniform(2.3, 2.8))  # Ajusta o delay conforme necessário
 
 def life():
     print('Verificando Life')
-    while programa.executando:  # Continua executando enquanto o programa estiver em execução
+    while programa.executando:
         if not programa.pausado:
             try:
-                if pg.pixelMatchesColor(605, 792, (69, 70, 70)):  # Passa os valores diretamente
-                    pg.press('1')
-            except pg.FailSafeException:  # Trata exceção quando o pixel não é encontrado
+                if pg.pixelMatchesColor(605, 792, (69, 70, 70)):
+                    pg.press('1') #hotkey pot vida
+            except pg.FailSafeException:
                 pass
-        time.sleep(1)  # Ajusta o delay conforme necessário
+        time.sleep(1) # Ajusta o delay conforme necessário
 
-# Restante do código
 class MeuPrograma:
     def __init__(self):
         self.executando = False
@@ -64,9 +62,9 @@ class MeuPrograma:
                 print("Programa iniciado")
 
                 threading.Thread(target=self.loop_kill_box).start()
-                threading.Thread(target=exura).start()  # Inicia a função exura em uma thread separada
-                threading.Thread(target=mana).start()   # Inicia a função mana em uma thread separada
-                threading.Thread(target=life).start()   # Inicia a função life em uma thread separada
+                threading.Thread(target=exura).start()  
+                threading.Thread(target=mana).start()   
+                threading.Thread(target=life).start()   
             elif key.char == 'p':
                 self.pausado = not self.pausado
                 print("Programa pausado" if self.pausado else "Programa retomado")
