23b4fd3c007e3fdeda88b5b28d56c706d38531f1
Milton Barroca <miltonbarroca@gmail.com>
2024-03-26 12:10:53 -0300

    vida cheia add

--- main.py
+++ main.py
@@ -30,6 +30,30 @@ def mana_check(image_path):
         print("Mana não está cheia. Pressionando 3...")
         pg.press('3')
 
+def encontrar_imagem(image_path):
+    screen = pg.screenshot()
+    screen_np = np.array(screen)
+    screen_gray = cv2.cvtColor(screen_np, cv2.COLOR_BGR2GRAY)
+    
+    template = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    w, h = template.shape[::-1]
+    
+    res = cv2.matchTemplate(screen_gray, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
+    
+    if loc[0].size > 0:
+        return True
+    else:
+        return False
+
+def mana_check(image_path):
+    if encontrar_imagem(image_path):
+        print("Vida está cheia!")
+    else:
+        print("Vida não está cheia. Pressionando 3...")
+        pg.press('1')
+
 class MeuPrograma:
     def __init__(self):
         self.executando = False
@@ -62,6 +86,7 @@ class MeuPrograma:
 
                 threading.Thread(target=self.loop_kill_box).start()
                 threading.Thread(target=self.mana_check_loop, args=('img/mana_cheia.jpg',)).start()
+                threading.Thread(target=self.mana_check_loop, args=('img/vida_cheia.jpg',)).start()
             elif key.char == 'p':
                 self.pausado = not self.pausado
                 print("Programa pausado" if self.pausado else "Programa retomado")
