5267f16855afe45c752957bf4e5b9946f4bb1502
Milton Barroca <miltonbarroca@gmail.com>
2024-03-26 12:20:36 -0300

    .

--- main.py
+++ main.py
@@ -30,28 +30,11 @@ def mana_check(image_path):
         print("Mana não está cheia. Pressionando 3...")
         pg.press('3')
 
-def encontrar_imagem(image_path):
-    screen = pg.screenshot()
-    screen_np = np.array(screen)
-    screen_gray = cv2.cvtColor(screen_np, cv2.COLOR_BGR2GRAY)
-    
-    template = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    w, h = template.shape[::-1]
-    
-    res = cv2.matchTemplate(screen_gray, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
-    loc = np.where(res >= threshold)
-    
-    if loc[0].size > 0:
-        return True
-    else:
-        return False
-
-def mana_check(image_path):
+def vida_check(image_path):
     if encontrar_imagem(image_path):
         print("Vida está cheia!")
     else:
-        print("Vida não está cheia. Pressionando 3...")
+        print("Vida não está cheia. Pressionando 1...")
         pg.press('1')
 
 class MeuPrograma:
@@ -78,6 +61,12 @@ class MeuPrograma:
                 mana_check(image_path)
             time.sleep(1)
 
+    def vida_check_loop(self, image_path):
+        while self.executando:
+            if not self.pausado:
+                vida_check(image_path)
+            time.sleep(1)
+
     def on_press(self, key):
         try:
             if key.char == '=' and not self.executando:
@@ -86,7 +75,7 @@ class MeuPrograma:
 
                 threading.Thread(target=self.loop_kill_box).start()
                 threading.Thread(target=self.mana_check_loop, args=('img/mana_cheia.jpg',)).start()
-                threading.Thread(target=self.mana_check_loop, args=('img/vida_cheia.jpg',)).start()
+                threading.Thread(target=self.vida_check_loop, args=('img/vida_cheia.jpg',)).start()
             elif key.char == 'p':
                 self.pausado = not self.pausado
                 print("Programa pausado" if self.pausado else "Programa retomado")
@@ -127,3 +116,5 @@ if __name__ == "__main__":
     programa = MeuPrograma()
     programa.iniciar()
     programa.listener.join()
+
+
